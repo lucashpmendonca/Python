4ca9f5e08a08ad08c97548c72325ef9353639d19
Lucas Pacheco <lucashpmendonca@gmail.com>
2020-01-15 01:05:39 -0300

    FIX BUGS , ADD MUTE SYSTEM OK , START PAGE IN PROGRESS

--- Jogos/Snake ClassVersion/Snake.py
+++ Jogos/Snake ClassVersion/Snake.py
@@ -17,15 +17,17 @@ RIGHT = 6
 global clock
 clock = pygame.time.Clock()
 
-#Classes
+global mute
+mute = False
+
 
 class Game():
-
     #Initialize 
 
     def __init__(self,width,height,clock):
 
         pygame.init()
+        
         #Caption window
         pygame.display.set_caption('Snake')
         self.width=width
@@ -35,6 +37,55 @@ class Game():
         self.background=pygame.Surface(self.screen.get_size())
         self.background.fill(BLACK)
         self.background.convert()
+        
+        def game_intro(self):
+
+            intro = True
+
+            while intro:
+                for event in pygame.event.get():
+                    #print(event)
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                        
+                mouse = pygame.mouse.get_pos()
+                '''
+                self.background.fill((255,255,255))
+                self.intro_font = pygame.font.Font('freesansbold.ttf',115)
+                self.intro_font_screen = self.intro_font.render ('click ou aperte ESPAÇO para começar', True , (0,0,0))
+                self.intro_font_rect = self.intro_font_screen.get_rect()
+                self.intro_font_rect.midtop = ((600/2),(600/2))
+                self.screen.blit(self.intro_font_screen,self.intro_font_rect)'''
+                pygame.draw.rect(self.screen, (0,255,0),(250,250,250,100))
+                pygame.draw.rect(self.screen, (0,255,0),(0,0,70,70)) #botao mute
+                
+                # click para mute
+                if mouse[0] > 0 and mouse[0] < 0+70 and mouse[1] > 0 and mouse[1] < 0+70:
+                    pygame.display.update()
+                    if pygame.mouse.get_pressed()[0]:
+                        mutee()
+                        pygame.draw.rect(self.screen, (255,255,0),(0,0,70,70))
+                        print(mute)
+                        
+                        
+                    
+
+                #clique para inicar o jogo
+                mouse = pygame.mouse.get_pos()
+                click = pygame.mouse.get_pressed()[0]
+                if mouse[0] > 250 and mouse[0] < 250+150 and mouse[1] > 250 and mouse[1] < 250+100:
+                        #self.intro_font_screen.fill((100,255,100))
+                        pygame.display.update()
+                        if pygame.mouse.get_pressed()[0]:
+                            #print(intro) #teste
+                            intro = False
+                            pass
+            pass
+        pass
+
+        game_intro(self)
+        
         #Score Card
         self.scorecard=pygame.draw.rect(self.background,GREEN,Rect([10,10],[self.width - 20,50]),1)
         #Game area draw
@@ -48,6 +99,8 @@ class Game():
         self.snake=Snake()
         self.food=Food(self.snake)
         self.bigfood=BigFood(self.snake)
+        self.sound = pygame.mixer.Sound('comeu.ogg')
+        self.intro = game_intro(self)
 
     def run(self):
         """Main Loop
@@ -113,11 +166,18 @@ class Game():
             if self.snake.head[0]==self.food.foodXY[0] and self.snake.head[1]==self.food.foodXY[1]:
                 self.food=Food(self.snake)
                 self.incPoints()
+                self.Sound()
                 
             else:
                 #removes the tail
                 self.snake.body.pop()
-                '''
+
+            #Eat BigFood
+            if self.snake.head[0]==self.bigfood.bigfoodXY[0] and self.snake.head[1]==self.bigfood.bigfoodXY[1]:
+                self.bigfood=BigFood(self.snake)
+                self.incPointsBig()
+                self.Sound()
+            '''
             for x in range(0,600,10): #desenha linhas verticais
                 pygame.draw.line(self.screen,(40,40,40), (x,0 ),(x,600))
             for y in range(0,600,10): #desenha linhas verticais
@@ -127,6 +187,7 @@ class Game():
             self.drawTime()
             self.drawSnake()
             self.drawFood()
+            self.drawBigFood()
             
             pygame.display.update()
             self.screen.blit(self.background,(0,0))
@@ -134,12 +195,28 @@ class Game():
 
         pygame.quit()
 
+    def Sound(self):
+        print(mute) #teste 
+        if not mute:
+            try:
+                self.sound.play()
+            except mute == True:
+                pass
+            pass
+            
+        pass
+
+    
+
     def incPoints(self):
         self.points=self.points+100
 
+    def incPointsBig(self):
+        self.points=self.points+200
+
     def drawScore(self):
-        surface = self.font.render("Points: "+str(self.points), True, GRAY)
-        self.screen.blit(surface, (15, 25))
+        surface = self.font.render("SCORE: "+str(self.points), True, GRAY)
+        self.screen.blit(surface, (600-200, 25))
 
     def drawTime(self):
         total_segs = pygame.time.get_ticks()/1000
@@ -150,7 +227,7 @@ class Game():
         contador = (str(int(horas))+':'+str(int(minutos))+':'+str(round(segs_restantes_final,1)))
 
         time = self.font.render ('TEMPO : '+str(contador), False , (255,255,255))
-        self.screen.blit(time, (200, 25))
+        self.screen.blit(time, (35, 25))
 
     def drawSnake(self):
         for x in self.snake.body:
@@ -160,7 +237,7 @@ class Game():
         pygame.draw.rect(self.screen,RED, Rect(self.food.foodXY,(10,10)))
         
     def drawBigFood(self):
-        pygame.draw.rect(self.screen,RED, Rect(self.bigfood.bigfoodXY,(40,40)))
+        pygame.draw.rect(self.screen,RED, Rect(self.bigfood.bigfoodXY,(20,20)))
     
 
 class Snake():
